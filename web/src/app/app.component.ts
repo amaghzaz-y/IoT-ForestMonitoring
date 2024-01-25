ff66c99ceb180407cc8e06f0a08d2544b00bc193
Yassine AMAGHZAZ <yassine.amaghzaz@gmail.com>
2024-01-25 17:33:03 +0100

    add material, ngx-charts

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'web';
