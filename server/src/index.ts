89d7e9dca83b8be6663f7a7bf8e14ae4130eb0ee
Yassine AMAGHZAZ <yassine.amaghzaz@gmail.com>
2024-02-05 16:55:15 +0100

    add types

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,26 +1,49 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+
+const prisma = new PrismaClient();
+
 import mqtt from "mqtt";
+import { Payload } from "./types";
+
+const app = new Hono();
+
 let client = mqtt.connect("mqtt://eu1.cloud.thethings.network:1883", {
-  username: "",
-  password: "",
+  username: process.env.TTN_USER,
+  password: process.env.TTN_MQTT,
   protocolVersion: 3,
 });
 
 client.on("connect", () => {
   client.subscribe("#", (err) => {
-    if (!err) {
+    if (err) {
       console.log(err);
     }
+    console.log("connected to TTN !");
   });
 });
 
 client.on("message", (topic, message) => {
-  let payload = JSON.parse(message.toString());
-  console.log(payload["uplink_message"]);
-});
+  let raw = JSON.parse(message.toString());
+  let payload: Payload = {
+    deviceId: raw["end_device_ids"]["device_id"],
+    devEui: raw["end_device_ids"]["dev_eui"],
+    receivedAt: raw["received_at"],
+    content: raw["uplink_message"]["decoded_payload"],
+  };
 
-const app = new Hono();
+  console.log(payload);
+
+  prisma.endDevice.create({
+    data: {
+      DevEUI: payload.devEui,
+      LastSeen: new Date().getTime().toString(),
+      Eui: payload.deviceId,
+    },
+  });
+});
 
 serve({
   fetch: app.fetch,
---
+++ server/src/types.ts
@@ -0,0 +1,6 @@
+export interface Payload {
+  deviceId: string;
+  devEui: string;
+  receivedAt: string;
+  content: string;
+}
