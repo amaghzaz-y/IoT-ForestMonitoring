e02d066969c05c0bbab64e1bc6d75a3e5da30958
Yassine AMAGHZAZ <yassine.amaghzaz@gmail.com>
2024-02-12 10:06:25 +0100

    add highcharts

--- web/src/app/charts/devices/devices.component.ts
+++ web/src/app/charts/devices/devices.component.ts
@@ -3,12 +3,33 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { HighchartsChartModule } from 'highcharts-angular';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-devices',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatCardModule, MatDividerModule],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    HighchartsChartModule,
+  ],
   templateUrl: './devices.component.html',
   styleUrl: './devices.component.css',
 })
-export class DevicesComponent {}
+export class DevicesComponent {
+  Highcharts: typeof Highcharts = Highcharts; // required
+  chartConstructor: string = 'chart';
+  chartOptions: Highcharts.Options = {
+    title: {
+      text: 'Total Devices Graph',
+    },
+    chart: {
+      backgroundColor: 'gray',
+    },
+  };
+  updateFlag: boolean = false;
+  constructor() {}
+}
